db5b48f1bf66b2fc441b962428f149fc69c7203b
iradiane <iradudiane0@gmail.com>
2024-02-26 13:17:07 +0200

    js

---
+++ day2/vr.js
@@ -0,0 +1,24 @@
+// -------typesof variables----
+// 1.numerical-datatypes
+var age=18; 
+var my_name="diane";
+console.log(my_name + age);
+console.log(`my name is ${my_name} i am ${age} years old`);
+// use of const
+// const age=18; 
+// const my_name="diane";
+// const isshecode=true;
+// console.log(my_name + age);
+// console.log(`my name is ${my_name} i am ${age} years old ${isshecode} yes`);
+const array=["diane",18,false];
+console.log(array);
+/* variable decration and variable definition*/
+// var x;
+// var x=20;
+/* different between let var and const*/
+function myname() {
+    let me="diane";
+    console.log(me);
+}
+myname();
+/* different datatype in js*/
