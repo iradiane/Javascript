292791fa63fc9416d58a785978c4caca4d380dd2
iradiane <iradudiane0@gmail.com>
2024-02-29 11:47:25 +0200

    day5

---
+++ day2/day3/day4/day5/math.js
@@ -0,0 +1,14 @@
+let num=23;
+console.log(Math.abs(num));
+let number=2.7
+console.log(Math.ceil(number));
+console.log(Math.round(number));
+console.log(Math.floor(number));
+console.log(Math.max(number,num));
+console.log(Math.min(number,num));
+console.log(Math.pow(2,2));
+let numb=16;
+console.log(Math.sqrt(numb));
+console.log(Math.random());
+
+
